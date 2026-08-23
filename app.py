cf47c2d860ab08ccfc1445d418c38c412a8cc170
hand-to-hand-money <ljuanriccardo@gmail.com>
2026-08-23 05:37:22 +0200

    Fix Flask app initialization syntax

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+app = flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 import bcrypt
