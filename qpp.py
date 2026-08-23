eae79a82e8e4ad3e9bab89e8c7ddc3bd3f51f39f
hand-to-hand-money <ljuanriccardo@gmail.com>
2026-08-23 15:18:14 +0200

    Fix Flask app initialization in qpp.py

--- qpp.py
+++ qpp.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-app = flask(__name__)
+app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 import bcrypt
 import uuid
 from datetime import datetime
 
-app = flask(__name__)
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'banque_secrete_2026'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banque.db'
 db = SQLAlchemy(app)
