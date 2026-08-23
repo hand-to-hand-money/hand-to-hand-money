a177c8b6950cfccaa83ec4e6ef7ab4428eebc2b0
hand-to-hand-money <ljuanriccardo@gmail.com>
2026-08-24 01:41:12 +0200

    Fix import formatting in qpp.py

--- qpp.py
+++ qpp.py
@@ -1,4 +1,4 @@
- from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 from flask import Response
